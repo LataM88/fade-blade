218858d527a2555fd7c1d79d22c2853759d604ea
Michał <latamichal88@gmail.com>
2025-12-05 11:20:13 +0100

    service section zrobione

--- src/app/components/landing/ServicesSection/ServicesSection.tsx
+++ src/app/components/landing/ServicesSection/ServicesSection.tsx
@@ -6,30 +6,71 @@ export default function ServicesSection() {
             <div className={styles.container}>
                 <h1>Our Services</h1>
             </div>
-            <div className={styles.services}>
-                <div className={styles.service1}>
-                    <h3>Classic Haircut</h3>
-                    <p>precise cut tailored to the client’s face shape and style.</p>
+            <div className={styles.mainWrapper}>
+                <div className={styles.services}>
+                    <div className={styles.serviceItem}>
+                        <div className={styles.leftContent}>
+                            <div className={styles.serviceImage}></div>
+                            <div className={styles.textContent}>
+                                <h3>Classic Haircut</h3>
+                                <p>precise cut tailored to the client’s face shape and style.</p>
+                            </div>
+                        </div>
+                        <div className={styles.servicePrice}>30€</div>
+                    </div>
+                    <div className={styles.serviceItem}>
+                        <div className={styles.leftContent}>
+                            <div className={styles.serviceImage}></div>
+                            <div className={styles.textContent}>
+                                <h3>Beard Trim & Shaping</h3>
+                                <p>beard styling, contour shaving, and care with balm or oil.</p>
+                            </div>
+                        </div>
+                        <div className={styles.servicePrice}>15€</div>
+                    </div>
+                    <div className={styles.serviceItem}>
+                        <div className={styles.leftContent}>
+                            <div className={styles.serviceImage}></div>
+                            <div className={styles.textContent}>
+                                <h3>Hot Towel Shave</h3>
+                                <p>traditional straight-razor shave with a hot towel and facial massage.</p>
+                            </div>
+                        </div>
+                        <div className={styles.servicePrice}>25€</div>
+                    </div>
+                    <div className={styles.serviceItem}>
+                        <div className={styles.leftContent}>
+                            <div className={styles.serviceImage}></div>
+                            <div className={styles.textContent}>
+                                <h3>Buzz Cut</h3>
+                                <p>quick and even clipper cut with one length.</p>
+                            </div>
+                        </div>
+                        <div className={styles.servicePrice}>20€</div>
+                    </div>
+                    <div className={styles.serviceItem}>
+                        <div className={styles.leftContent}>
+                            <div className={styles.serviceImage}></div>
+                            <div className={styles.textContent}>
+                                <h3>VIP Package</h3>
+                                <p>haircut + beard + grooming + head massage</p>
+                            </div>
+                        </div>
+                        <div className={styles.servicePrice}>55€</div>
+                    </div>
+                    <div className={styles.serviceItem}>
+                        <div className={styles.leftContent}>
+                            <div className={styles.serviceImage}></div>
+                            <div className={styles.textContent}>
+                                <h3>Camouflage/Hair or Beard Coloring</h3>
+                                <p>subtle gray blending for a natural look.</p>
+                            </div>
+                        </div>
+                        <div className={styles.servicePrice}>40€</div>
+                    </div>
                 </div>
-                <div className={styles.service2}>
-                    <h3>Beard Trim & Shaping</h3>
-                    <p>beard styling, contour shaving, and care with balm or oil.</p>
-                </div>
-                <div className={styles.service3}>
-                    <h3>Hot Towel Shave</h3>
-                    <p>traditional straight-razor shave with a hot towel and facial massage.</p>
-                </div>
-                <div className={styles.service4}>
-                    <h3>Buzz Cut</h3>
-                    <p>quick and even clipper cut with one length.</p>
-                </div>
-                <div className={styles.service5}>
-                    <h3>VIP Package</h3>
-                    <p>haircut + beard + grooming + head massage</p>
-                </div>
-                <div className={styles.service6}>
-                    <h3>Camouflage/Hair or Beard Coloring</h3>
-                    <p>subtle gray blending for a natural look.</p>
+                <div className={styles.rightContent}>
+                    <div className={styles.rightContentImage}></div>
                 </div>
             </div>
         </section>
