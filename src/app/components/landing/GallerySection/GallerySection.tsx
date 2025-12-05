6b55f57f86d9fdd05f951d658a03e9dd8f870f80
Michał <latamichal88@gmail.com>
2025-12-05 13:11:11 +0100

    gallery section

--- src/app/components/landing/GallerySection/GallerySection.tsx
+++ src/app/components/landing/GallerySection/GallerySection.tsx
@@ -1,10 +1,26 @@
 import styles from './GallerySection.module.css';
+import Button from '../../ui/Button';
 
 export default function GallerySection() {
     return (
         <section className={styles.section}>
             <div className={styles.container}>
-                <h2>Gallery</h2>
+                <h1>Gallery</h1>
+            </div>
+            <div className={styles.upperWrapper}>
+                <div className={styles.upperImage1}></div>
+                <div className={styles.upperImage2}></div>
+                <div className={styles.upperImage3}></div>
+                <div className={styles.upperImage4}></div>
+            </div>
+            <div className={styles.lowerWrapper}>
+                <div className={styles.lowerImage1}></div>
+                <div className={styles.lowerImage2}></div>
+                <div className={styles.lowerImage3}></div>
+                <div className={styles.lowerImage4}></div>
+            </div>
+            <div className={styles.btnWrapper}>
+                <Button variant="gallery">See more photos</Button>
             </div>
         </section>
     );
--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -2,7 +2,7 @@ import styles from './Button.module.css';
 import React, { ButtonHTMLAttributes } from 'react';
 
 type ButtonProps = {
-    variant?: 'book-appointment' | 'portfolio' | 'reservation' | 'login';
+    variant?: 'book-appointment' | 'portfolio' | 'gallery' | 'login';
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button: React.FC<ButtonProps> = ({ children, variant = 'book-appointment', ...props }) => {
